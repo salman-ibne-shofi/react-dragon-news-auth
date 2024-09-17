788bddd81a964187082cea98a952f6ffc104ce96
salman-ibne-shofi <salmanibnshafi@gmail.com>
2024-09-18 03:57:05 +0600

    custom tailwind class and font setup

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,14 @@
-
+import Header from "../Shared/Header/Header";
+import Navbar from "../Shared/Navbar/Navbar";
 
 const Home = () => {
-    return (
-        <div>
-            <h2 className="text-3xl">This is home</h2>
-        </div>
-    );
+	return (
+		<div>
+			<Header></Header>
+			<Navbar></Navbar>
+			<h2 className="text-3xl font-poppins font-bold">This is home</h2>
+		</div>
+	);
 };
 
 export default Home;
