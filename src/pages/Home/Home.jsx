354ace41573c78efbb596d0c6d56359ad9b077d5
salman-ibne-shofi <salmanibnshafi@gmail.com>
2024-09-18 04:24:48 +0600

    homepage layout and date time formatting using moment.js

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,24 @@
 import Header from "../Shared/Header/Header";
+import LeftSideNav from "../Shared/LeftSideNav/LeftSideNav";
 import Navbar from "../Shared/Navbar/Navbar";
+import RightSideNav from "../Shared/RightSideNav/RightSideNav";
 
 const Home = () => {
 	return (
 		<div>
 			<Header></Header>
 			<Navbar></Navbar>
-			<h2 className="text-3xl font-poppins font-bold">This is home</h2>
+			<div className="grid grid-cols-1 md:grid-cols-4 gap-6">
+				<div className="border">
+					<LeftSideNav></LeftSideNav>
+				</div>
+				<div className="md:col-span-2 border">
+					<h2 className="text-4xl">News comming soon...</h2>
+				</div>
+				<div className="border">
+					<RightSideNav></RightSideNav>
+				</div>
+			</div>
 		</div>
 	);
 };
