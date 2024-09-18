39a44b7d17c46c7f96e27ee25d9c702833331829
salman-ibne-shofi <salmanibnshafi@gmail.com>
2024-09-19 05:29:42 +0600

    left side navigation and module summary

--- src/pages/Shared/LeftSideNav/LeftSideNav.jsx
+++ src/pages/Shared/LeftSideNav/LeftSideNav.jsx
@@ -1,7 +1,27 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
 const LeftSideNav = () => {
+	const [categories, setCateGories] = useState([]);
+
+	useEffect(() => {
+		fetch("categories.json")
+			.then((res) => res.json())
+			.then((data) => setCateGories(data));
+	}, []);
+
 	return (
-		<div>
-			<h2 className="text-2xl">Left Side</h2>
+		<div className="space-y-6">
+			<h2 className="text-2xl">All Categories</h2>
+			{categories.map((category) => (
+				<Link
+					className="block ml-4 text-xl font-semibold"
+					key={category.id}
+					to={`/category/${category.id}`}
+				>
+					{category.name}
+				</Link>
+			))}
 		</div>
 	);
 };
